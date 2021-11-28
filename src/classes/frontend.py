9337364eafd87bdc641c08a9f5dc5501a539fbf6
Ethan Clarke <ethan.clarke42@mail.dcu.ie>
2021-11-28 17:46:57 +0000

    Most basic UI added. has a main menu that goes to a blank game screen

--- src/classes/frontend.py
+++ src/classes/frontend.py
@@ -1,9 +1,10 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix image import Image
+from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
 from backend import *
 
 
@@ -44,9 +45,22 @@ class PotDisplay(Pot):
 
 class DublinHoldEm(App):
     def build(self):
+        """
+        Builds the opening window for the game.
+        """
+        Window.clearcolor = (1,1,1,1)
         self.window = GridLayout()
         self.window.cols = 1
 
-        self.window.add_widget(Image(source="../assets/homepage.png")
+        self.window.add_widget(Image(source="../assets/homepage.png"))
+
+        self.button = Button(text="PLAY")
+        self.button.bind(on_press=self.game_window)
+        self.window.add_widget(self.button)
 
         return self.window
+
+    def game_window(self, instance):
+        self.window.clear_widgets()
+
+        self.window.add_widget(Image(source="../assets/basebackground.png"))
