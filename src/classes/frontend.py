83558c0ee885103bd344697ed5c66adc22060c26
Ethan Clarke <ethan.clarke42@mail.dcu.ie>
2021-11-29 13:03:43 +0000

    Main menu with a "play" button added. It leads to a blank game window rn

--- src/classes/frontend.py
+++ src/classes/frontend.py
@@ -1,5 +1,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from kivy.config import Config
+
 from backend import *
 
 
@@ -39,10 +45,20 @@ class PotDisplay(Pot):
         """
 
 
-class DublinHoldEm(Widget):
+class StartScreen(Screen):
+    pass
+
+
+class GameScreen(Screen):
+    pass
+
+
+class RootScreen(ScreenManager):
     pass
 
 
 class PokerApp(App):
     def build(self):
-        return DublinHoldEm()
+        Config.set("graphics", "width", "1350")
+        Config.set("graphics", "height", "650")
+        return RootScreen()
